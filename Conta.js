1a410c773222ab5552e5315767119b6e06ac8878
klysmamsilva <87070813+klysmamsilva@users.noreply.github.com>
2021-12-22 16:56:06 -0300

    Update Conta.js

--- Conta.js
+++ Conta.js
@@ -19,6 +19,27 @@ class Conta{
             return;
         }
         this._saldo = this._saldo + valor;
+    }class Conta{    
+    agencia;
+    numero;
+    cliente;
+    _saldo = 0.0;
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo = this._saldo - valor;
+        }        
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo = this._saldo + valor;
     }
 
     transferir(valor, contaDestino){
