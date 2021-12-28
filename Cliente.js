297540ad2a68e39ceace8887570d3729d5fa3f06
klysmamsilva <87070813+klysmamsilva@users.noreply.github.com>
2021-12-28 10:27:17 -0300

    Update Cliente.js

--- Cliente.js
+++ Cliente.js
@@ -1,6 +1,25 @@
-class Cliente{
+class Cliente {
     nome;
     cpf;
+    fone;
+    endereco;
+    codigo
+
+    criar() {
+
+    }
+
+    destruir() {
+
+    }
+
+    fecharConta() {
+
+    }
+
+    abrirConta() {
+
+    }
 }
 
 module.exports = Cliente;
