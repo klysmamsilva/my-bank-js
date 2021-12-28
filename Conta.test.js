38471db4fb658032246ff174d86b7acc0c27594e
klysmamsilva <87070813+klysmamsilva@users.noreply.github.com>
2021-12-28 10:31:39 -0300

    Update Conta.test.js

--- Conta.test.js
+++ Conta.test.js
@@ -7,7 +7,7 @@ test('teste saque valor igual ao saldo', () => {
     cliente01.cpf = '11100099954';
     var contaDoCliente01 = new Conta();
     contaDoCliente01.agencia = 1101;
-    contaDoCliente01.numero = 2001;    
+    contaDoCliente01.numero = 2001;
     contaDoCliente01.cliente = cliente01;
 
     contaDoCliente01.depositar(100.0);
@@ -22,7 +22,7 @@ test('teste saque valor maior que o saldo', () => {
     cliente01.cpf = '11100099954';
     var contaDoCliente01 = new Conta();
     contaDoCliente01.agencia = 1101;
-    contaDoCliente01.numero = 2001;    
+    contaDoCliente01.numero = 2001;
     contaDoCliente01.cliente = cliente01;
 
     contaDoCliente01.depositar(100.0);
@@ -37,7 +37,7 @@ test('teste deposito valor maior que o saldo', () => {
     cliente01.cpf = '11100099954';
     var contaDoCliente01 = new Conta();
     contaDoCliente01.agencia = 1101;
-    contaDoCliente01.numero = 2001;    
+    contaDoCliente01.numero = 2001;
     contaDoCliente01.cliente = cliente01;
 
     contaDoCliente01.depositar(200.0);
@@ -51,7 +51,7 @@ test('teste deposito valor negativo', () => {
     cliente01.cpf = '11100099954';
     var contaDoCliente01 = new Conta();
     contaDoCliente01.agencia = 1101;
-    contaDoCliente01.numero = 2001;    
+    contaDoCliente01.numero = 2001;
     contaDoCliente01.cliente = cliente01;
 
     contaDoCliente01.depositar(-200.0);
@@ -59,3 +59,142 @@ test('teste deposito valor negativo', () => {
     expect(contaDoCliente01.saldo).toBe(0);
 });
 
+var cliente1 = new Cliente();
+var cliente2 = new Cliente();
+cliente1.nome = 'Jose da silva';
+cliente1.cpf = '11100099954';
+cliente2.nome = 'Zé fina';
+cliente2.cpf = '12345678985';
+contaDoCliente1 = new Conta();
+contaDoCliente1.agencia = 1101;
+contaDoCliente1.numero = 2001;
+contaDoCliente1.cliente = cliente1;
+
+contaDoCliente2 = new Conta();
+contaDoCliente2.agencia = 1101;
+contaDoCliente2.numero = 1992;
+contaDoCliente2.cliente = cliente2;
+
+contaDoCliente1.depositar(100.0);
+contaDoCliente2.depositar(50.0);
+
+test('teste transferir 50 reais da contaA para contaB', () => {
+    var cliente1 = new Cliente();
+    var cliente2 = new Cliente();
+    cliente1.nome = 'Jose da silva';
+    cliente1.cpf = '11100099954';
+    cliente2.nome = 'Zé fina';
+    cliente2.cpf = '12345678985';
+    contaDoCliente1 = new Conta();
+    contaDoCliente1.agencia = 1101;
+    contaDoCliente1.numero = 2001;
+    contaDoCliente1.cliente = cliente1;
+
+    contaDoCliente2 = new Conta();
+    contaDoCliente2.agencia = 1101;
+    contaDoCliente2.numero = 1992;
+    contaDoCliente2.cliente = cliente2;
+
+    contaDoCliente1.depositar(100.0);
+    contaDoCliente2.depositar(50.0);
+
+    contaDoCliente1.transferir(50.0, contaDoCliente2);
+
+    expect(contaDoCliente1.saldo).toBe(50.0);
+    expect(contaDoCliente2.saldo).toBe(100.0);
+});
+
+test('teste transferir 50 reais da contaB para contaA', () => {
+    var cliente1 = new Cliente();
+    var cliente2 = new Cliente();
+    cliente1.nome = 'Jose da silva';
+    cliente1.cpf = '11100099954';
+    cliente2.nome = 'Zé fina';
+    cliente2.cpf = '12345678985';
+    contaDoCliente1 = new Conta();
+    contaDoCliente1.agencia = 1101;
+    contaDoCliente1.numero = 2001;
+    contaDoCliente1.cliente = cliente1;
+
+    contaDoCliente2 = new Conta();
+    contaDoCliente2.agencia = 1101;
+    contaDoCliente2.numero = 1992;
+    contaDoCliente2.cliente = cliente2;
+
+    contaDoCliente1.depositar(50.0);
+    contaDoCliente2.depositar(100.0);
+
+    contaDoCliente2.transferir(50.0, contaDoCliente1);
+
+    expect(contaDoCliente1.saldo).toBe(100.0);
+    expect(contaDoCliente2.saldo).toBe(50.0);
+});
+
+test('teste transferir -100 reais da contaA para contaB', () => {
+    var cliente1 = new Cliente();
+    var cliente2 = new Cliente();
+    cliente1.nome = 'Jose da silva';
+    cliente1.cpf = '11100099954';
+    cliente2.nome = 'Zé fina';
+    cliente2.cpf = '12345678985';
+    contaDoCliente1 = new Conta();
+    contaDoCliente1.agencia = 1101;
+    contaDoCliente1.numero = 2001;
+    contaDoCliente1.cliente = cliente1;
+
+    contaDoCliente2 = new Conta();
+    contaDoCliente2.agencia = 1101;
+    contaDoCliente2.numero = 1992;
+    contaDoCliente2.cliente = cliente2;
+
+    contaDoCliente1.depositar(100.0);
+    contaDoCliente2.depositar(50.0);
+
+    contaDoCliente1.transferir(-100.0, contaDoCliente2);
+
+    expect(contaDoCliente1.saldo).toBe(100.0);
+    expect(contaDoCliente2.saldo).toBe(50.0);
+});
+
+test('teste transferir 500 reais da contaB para contaA', () => {
+    var cliente1 = new Cliente();
+    var cliente2 = new Cliente();
+    cliente1.nome = 'Jose da silva';
+    cliente1.cpf = '11100099954';
+    cliente2.nome = 'Zé fina';
+    cliente2.cpf = '12345678985';
+    contaDoCliente1 = new Conta();
+    contaDoCliente1.agencia = 1101;
+    contaDoCliente1.numero = 2001;
+    contaDoCliente1.cliente = cliente1;
+
+    contaDoCliente2 = new Conta();
+    contaDoCliente2.agencia = 1101;
+    contaDoCliente2.numero = 1992;
+    contaDoCliente2.cliente = cliente2;
+
+    contaDoCliente1.depositar(100.0);
+    contaDoCliente2.depositar(50.0);
+
+    contaDoCliente2.transferir(500.0, contaDoCliente1);
+
+    expect(contaDoCliente1.saldo).toBe(100.0);
+    expect(contaDoCliente2.saldo).toBe(50.0);
+});
+
+test('teste transferir 100 reais da contaA para contaA', () => {
+    var cliente1 = new Cliente();
+    cliente1.nome = 'Jorge';
+    cliente1.cpf = '1110099524';
+
+    contaDoCliente1 = new Conta();
+    contaDoCliente1.agencia = 1101;
+    contaDoCliente1.numero = 2001;
+    contaDoCliente1.cliente = cliente1;
+
+    contaDoCliente1.depositar(100.0);
+
+    contaDoCliente1.transferir(100.0, contaDoCliente1);
+
+    expect(contaDoCliente1.saldo).toBe(100.0);
+});
